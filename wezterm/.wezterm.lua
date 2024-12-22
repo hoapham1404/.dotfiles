42a0be76169bf9013f16d683acd9214dd9200c3e
hoapham2k2 <20520995@gm.uit.edu.vn>
2024-12-23 02:07:12 +0700

    add wezterm

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,29 @@
+local wezterm = require 'wezterm'
+local mux = wezterm.mux
+local config = wezterm.config_builder()
+
+wezterm.on("gui-startup", function()
+	local tab, pane, window = mux.spawn_window {}
+	window:gui_window():maximize()
+end)
+
+config.color_scheme = 'Dracula'
+
+config.window_padding = {
+	left = 2,
+	right = 2,
+	top = 0,
+	bottom = 0,
+}
+
+config.enable_tab_bar = false
+config.initial_rows = 100
+config.initial_cols = 205
+config.window_decorations = "NONE"
+
+config.window_background_opacity = 0.75
+config.font_size = 18.0
+
+config.default_prog = { '/usr/bin/fish', '-l' }
+
+return config
