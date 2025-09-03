0680d924235130bd8cfe53f7b8825dd1ac6bb570
Pham Quang Hoa <hoapq@allexceed.vn>
2025-09-03 08:55:08 +0700

    update fzf

--- readonly_Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -17,3 +17,39 @@ Set-PSReadLineKeyHandler -Key Ctrl+f -Function ForwardChar
 Set-PSReadLineKeyHandler -Key Ctrl+b -Function BackwardChar
 Set-PSReadLineKeyHandler -Key Ctrl+a -Function BeginningOfLine
 Set-PSReadLineKeyHandler -Key Ctrl+e -Function EndOfLine
+
+Set-PSReadLineKeyHandler -Key 'Ctrl+a,f'  -ScriptBlock {
+    # Directories to search
+    $dirs =  @(
+        "$HOME\Workspace",
+        "D:\Workspace",
+        "$HOME\Documents",
+        "$HOME\Workspace"
+    )
+
+    # Find directories up to depth 3
+    $foundDirs = foreach ($base in $dirs) {
+        if (Test-Path $base) {
+            Get-ChildItem -Path $base -Directory -Recurse -Depth 3 | Select-Object -ExpandProperty FullName
+        }
+    }
+
+    if (-not $foundDirs) {
+        Write-Host "No directories found."
+        exit 0
+    }
+
+    # Try fzf first then fallback to Out-GridView
+    if (Get-Command fzf -ErrorAction SilentlyContinue) {
+        $Selected = $foundDirs | fzf
+    } else {
+        $Selected = $foundDirs | Out-GridView -Title "Select a directory" -PassThru
+    }
+
+    # Exit if nothing selected
+    if (-not $Selected) {
+        exit 0
+    }
+
+    Set-Location -Path $Selected
+}
